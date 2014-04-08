b6535afa18db12faaa0cb6d2becd0366d9415699
TomSlick08 <tommy.threadcraft@gmail.com>
2014-04-08 17:46:02 -0400

    all test working/red

--- Redt/spec/models/link_spec.rb
+++ Redt/spec/models/link_spec.rb
@@ -2,10 +2,10 @@ require 'spec_helper'
 
 
 describe Link do
-  subject(:Link){Link.new(
-		link_url: 'Dash.com'
+  subject(:Link){Link.new({
+		link_url: 'Dash.com',
 		user_id: 1
-  	}) 
+  	})} 
   it{ should validate_presence_of(:link_url)}
   it{ should validate_presence_of(:user_id)}
 	it{ should validate_uniqueness_of(:link_url)}
--- Redt/spec/models/user_spec.rb
+++ Redt/spec/models/user_spec.rb
@@ -1,11 +1,11 @@
 require 'spec_helper'
 
 describe User do
-  subject(:User){User.new(
-		name: 'Dame'
-  	email: 'Dash@dash.com'
+  subject(:User){User.new({
+		name: 'Dame',
+  	email: 'Dash@dash.com',
   	password_digest: 'dash'
-  	}) 
+  	})} 
   it{ should validate_presence_of(:name)}
   it{ should validate_presence_of(:email)}
 	it{ should validate_presence_of(:password_digest)}
